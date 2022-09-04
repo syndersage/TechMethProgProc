29177ca3dd83fc7e3206bec8c9d0bd5b3efa6f42
syndersage <syndersage@gmail.com>
2022-09-05 06:43:39 +0700

    Added tests, the same as in TechMethProgOOP project.

---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,101 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import tmp.proc.Client;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Client tests")
+public class ClientTest {
+
+    @BeforeEach
+    void clearOutputFile() throws IOException {
+        Files.writeString(Path.of("F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\out"), "");
+        Files.writeString(Path.of("F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\out2"), "");
+        Files.writeString(Path.of("F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\out3.txt"), "");
+        Files.writeString(Path.of("F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\emptyOut"), "");
+        Client.arguments.help = false;
+        Client.arguments.verbose = false;
+        Client.arguments.pair = false;
+        Client.arguments.sort = false;
+    }
+
+    @Test
+    void argumentTest() {
+        Client.main(new String[]{"-p", "-i", "F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\emptyIn", "-s", "-o", "F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgOOP\\src\\test\\java\\ClientTestData\\emptyOut", "-v"});
+        assertTrue(Client.arguments.verbose);
+        assertTrue(Client.arguments.pair);
+        assertTrue(Client.arguments.sort);
+    }
+
+    @Test
+    void argument2Test() {
+        Client.main(new String[]{"input.txt", "output.txt", "йцу"});
+        assertFalse(Client.arguments.verbose);
+    }
+
+    @Test
+    void argument3Test() {
+        Client.main(new String[]{"F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\in", "F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\out"});
+        assertFalse(Client.arguments.verbose);
+    }
+
+    @Test
+    void correctPrintWriteTest() throws IOException {
+        Client.main(new String[]{"-i", "F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\in", "-o", "F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\out", "-v", "-s", "-p"});
+        String str = """
+                Filled container.\r
+                Container contains 2 elements.\r
+                1: Aphorism: text. By: author. Rating score: 5\r
+                2: Proverb: text3. From: country. Rating score: 7\r
+                \r
+                Iterating every pair:\r
+                Aphorism and Proverb\r
+                \r
+                Empty container.\r
+                Container contains 0 elements.\r
+                """;
+        assertEquals(str, Files.readString(Path.of("F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\out")));
+    }
+
+    @Test
+    void correctPrintWrite2Test() throws IOException {
+        Client.main(new String[]{"-i", "F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\in2", "-o", "F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\out2", "-v", "-s"});
+        String str = """
+                Filled container.\r
+                Container contains 3 elements.\r
+                1: Proverb: Still waters run deep. From: England. Rating score: 5\r
+                2: Proverb: Не плюй в колодец, пригодится воды напиться. From: Россия. Rating score: 4\r
+                3: Aphorism: Не беспокойся о том, что тебя не знают. Беспокойся о том, достоин ли ты того, чтобы тебя знали.. By: Конфуций. Rating score: 10\r
+                \r
+                Empty container.\r
+                Container contains 0 elements.\r
+                """;
+        assertEquals(str, Files.readString(Path.of("F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\out2")));
+    }
+
+    @Test
+    void correctPrintWrite3Test() throws IOException {
+        Client.main(new String[]{"-i", "F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\in2", "-o", "F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\out2", "-p", "-v"});
+        String str = """
+                Filled container.\r
+                Container contains 3 elements.\r
+                1: Aphorism: Не беспокойся о том, что тебя не знают. Беспокойся о том, достоин ли ты того, чтобы тебя знали.. By: Конфуций. Rating score: 10\r
+                2: Proverb: Не плюй в колодец, пригодится воды напиться. From: Россия. Rating score: 4\r
+                3: Proverb: Still waters run deep. From: England. Rating score: 5\r
+                \r
+                Iterating every pair:\r
+                Aphorism and Proverb\r
+                Aphorism and Proverb\r
+                Proverb and Proverb\r
+                \r
+                Empty container.\r
+                Container contains 0 elements.\r
+                """;
+        assertEquals(str, Files.readString(Path.of("F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgProc\\src\\test\\java\\ClientTestData\\out2")));
+    }
+}
---
+++ src/test/java/ContainerTest.java
@@ -0,0 +1,304 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import tmp.proc.Client;
+import tmp.proc.SingleLinkedContainer;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("SingleLinkedContainer tests")
+public class ContainerTest {
+
+    SingleLinkedContainer slc = new SingleLinkedContainer();
+
+    StringWriter output = new StringWriter();
+
+    String input;
+
+    @BeforeEach
+    public void initEach() {
+        output = new StringWriter();
+        SingleLinkedContainer.clear(slc);
+    }
+
+    @Test
+    void emptyListTest() {
+        assertEquals(0, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void emptyInputTest() {
+        String str = "";
+        SingleLinkedContainer.in(slc, new Scanner(str));
+        assertEquals(0, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void addElementTest() {
+        input = "1\nauthor\ntext\n5";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        assertEquals(1, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void clearListTest() {
+        input = "1\nauthor\ntext\n5";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.clear(slc);
+        assertEquals(0, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void clearList2Test() {
+        input = "1\nauthor\ntext\n5\n2\ncountry\ntext2\n1\nauthor2\ntext3";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.clear(slc);
+        assertEquals(0, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void clearListFieldsTest() {
+        input = "1\nauthor\ntext\n2\ncountry\ntext2\n1\nauthor2\ntext3";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.clear(slc);
+        assertNull(SingleLinkedContainer.getHead(slc));
+    }
+
+    @Test
+    void clearListFields2Test() {
+        input = "1\nauthor\ntext\n2\ncountry\ntext2\n1\nauthor2\ntext3";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.clear(slc);
+        assertNull(SingleLinkedContainer.getTail(slc));
+    }
+
+    @Test
+    void addTwoElementsTest() {
+        input = "1\nauthor\ntext\n5\n2\ncountry\ntext\n10";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        assertEquals(2, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void addThreeElementsTest() {
+        input = "1\nauthor\ntext\n5\n2\ncountry\ntext2\n10\n1\nauthor2\ntext3\n1";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        assertEquals(3, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void emptyLinesTest() {
+        input = "\n\n\n1\nauthor\ntext\n5\n2\ncountry\ntext\n10";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        assertEquals(2, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void spacesInWisdomTypeLineTest() {
+        input = "  \t1\r\nauthor\ntext\n5\n2\ncountry\ntext\n10";
+        Client.arguments.verbose = true;
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        assertEquals(2, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void spacesInWisdomTypeLine2Test() {
+        input = " \r 1               \nauthor\ntext\n5\n2\ncountry\ntext\n10";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        assertEquals(2, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void incorrectWisdomTypeTest() {
+        input = "\n\n\n4\nauthor\ntext\n5\n2\ncountry\ntext\n10";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        assertEquals(1, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void emptyLineBetweenTypeAndTextTest() {
+        input = "1\n\ntext\n5\n2\ncountry\ntext\n10";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        assertEquals(1, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void oneEmptyTextTest() {
+        input = "1\nauthor\n\n5";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        assertEquals(0, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void twoEmptyTextAndRateTest() {
+        input = "1\nauthor\n\n\n3\nanswer\ntext\n8"; //символ "2" будет являться текстом для первой мудрости
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        assertEquals(1, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void nonExistingFileTest() {
+        Path file = Path.of("ghdfjghjkdfgujdf");
+        NoSuchFileException exception = assertThrows(NoSuchFileException.class, () -> SingleLinkedContainer.in(slc, new Scanner(file)));
+        assertEquals("ghdfjghjkdfgujdf", exception.getMessage());
+    }
+
+    @Test
+    void existingFileTest() {
+        Path file = Path.of("F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgOOP\\src\\test\\java\\ContainerTestData\\in");
+        assertDoesNotThrow(() -> SingleLinkedContainer.in(slc, new Scanner(file)));
+    }
+
+    @Test
+    void addToListFromFileTest() throws IOException {
+        Path file = Path.of("F:\\Пользователи\\Pavel\\IdeaProjects\\TechMethProgOOP\\src\\test\\java\\ContainerTestData\\in");
+        Files.writeString(file, "1\nauthor\ntext\n5\n2\ncountry\ntext\n7");
+        SingleLinkedContainer.in(slc, new Scanner(file));
+        assertEquals(2, SingleLinkedContainer.getSize(slc));
+    }
+
+    @Test
+    void stringInputStringOutputTest() {
+        input = "1\nauthor\ntext\n2";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.out(slc, new PrintWriter(output));
+        assertEquals(output.toString(), "1: Aphorism: text. By: author. Rating score: 2\r\n");
+    }
+
+    @Test
+    void sortOneNodeTest() {
+        input = "1\nauthor\ntext\n2";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.sort(slc);
+        SingleLinkedContainer.out(slc, new PrintWriter(output));
+        assertEquals(output.toString(), "1: Aphorism: text. By: author. Rating score: 2\r\n");
+    }
+
+    @Test
+    void sortAlreadyCorrectOrderOfTwoNodesTest() {
+        input = "1\nauthor\ntext\n2\n2\ncountry\ntext!2\n8";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.sort(slc);
+        SingleLinkedContainer.out(slc, new PrintWriter(output));
+        assertEquals(output.toString(), """
+                1: Aphorism: text. By: author. Rating score: 2\r
+                2: Proverb: text!2. From: country. Rating score: 8\r
+                """);
+    }
+
+    @Test
+    void sortTwoSameCompareValueNodesTest() {
+        input = "1\nauthor\ntext\n2\n2\ncountry\ntext2\n8";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.sort(slc);
+        SingleLinkedContainer.out(slc, new PrintWriter(output));
+        assertEquals(output.toString(), """
+                1: Aphorism: text. By: author. Rating score: 2\r
+                2: Proverb: text2. From: country. Rating score: 8\r
+                """);
+    }
+
+    @Test
+    void sortIncorrectOrderOfTwoNodesTest() {
+        input = "1\nauthor\ntext\n2\n2\ncountry\ntext2\n8";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.sort(slc);
+        SingleLinkedContainer.out(slc, new PrintWriter(output));
+        assertEquals(output.toString(), """
+                1: Aphorism: text. By: author. Rating score: 2\r
+                2: Proverb: text2. From: country. Rating score: 8\r
+                """);
+    }
+
+    @Test
+    void sortFiveElementsTest() {
+        input = "1\nauthor\nte!!xt\n2\n2\ncountry\ntext2\n8\n3\nanswer\nte!!!!!xt2\n6\n2\ncountry\nte!xt2\n8\n1\nauthor\nte!!!xt2\n9";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.sort(slc);
+        SingleLinkedContainer.out(slc, new PrintWriter(output));
+        assertEquals(output.toString(), """
+                1: Proverb: text2. From: country. Rating score: 8\r
+                2: Proverb: te!xt2. From: country. Rating score: 8\r
+                3: Aphorism: te!!xt. By: author. Rating score: 2\r
+                4: Aphorism: te!!!xt2. By: author. Rating score: 9\r
+                5: Riddle: te!!!!!xt2. Answer: answer. Rating score: 6\r
+                """);
+    }
+
+    @Test
+    void emptyLinesAtBeginningTest() {
+        input = "\n\n\n\n\n\n\n\n1\nauthor\nte!!xt\n2\n2\ncountry\ntext2\n8\n3\nanswer\nte!!!!!xt2\n6\n2\ncountry\nte!xt2\n8\n1\nauthor\nte!!!xt2\n9";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.sort(slc);
+        SingleLinkedContainer.out(slc, new PrintWriter(output));
+        assertEquals(output.toString(), """
+                1: Proverb: text2. From: country. Rating score: 8\r
+                2: Proverb: te!xt2. From: country. Rating score: 8\r
+                3: Aphorism: te!!xt. By: author. Rating score: 2\r
+                4: Aphorism: te!!!xt2. By: author. Rating score: 9\r
+                5: Riddle: te!!!!!xt2. Answer: answer. Rating score: 6\r
+                """);
+    }
+
+    @Test
+    void emptyLinesAtStartAndBetweenWisdomElementsTest() {
+        input = "\n\n\n\n\n\n\n\n1\nauthor\nte!!xt\n2\n\n\n\n\n2\ncountry\ntext2\n8\n3\nanswer\nte!!!!!xt2\n6\n\n\n\n\n2\ncountry\nte!xt2\n8\n\n\n1\nauthor\nte!!!xt2\n9";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.sort(slc);
+        SingleLinkedContainer.out(slc, new PrintWriter(output));
+        assertEquals(output.toString(), """
+                1: Proverb: text2. From: country. Rating score: 8\r
+                2: Proverb: te!xt2. From: country. Rating score: 8\r
+                3: Aphorism: te!!xt. By: author. Rating score: 2\r
+                4: Aphorism: te!!!xt2. By: author. Rating score: 9\r
+                5: Riddle: te!!!!!xt2. Answer: answer. Rating score: 6\r
+                """);
+    }
+
+    @Test
+    void emptyLinesAtTheEndTest() {
+        input = "1\nauthor\nte!!xt\n2\n2\ncountry\ntext2\n8\n3\nanswer\nte!!!!!xt2\n6\n2\ncountry\nte!xt2\n8\n1\nauthor\nte!!!xt2\n9\n\n\n\n\n    ";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.sort(slc);
+        SingleLinkedContainer.out(slc, new PrintWriter(output));
+        assertEquals(output.toString(), """
+                1: Proverb: text2. From: country. Rating score: 8\r
+                2: Proverb: te!xt2. From: country. Rating score: 8\r
+                3: Aphorism: te!!xt. By: author. Rating score: 2\r
+                4: Aphorism: te!!!xt2. By: author. Rating score: 9\r
+                5: Riddle: te!!!!!xt2. Answer: answer. Rating score: 6\r
+                """);
+    }
+
+    @Test
+    void blankLinesBeforeAfterBetweenWisdomElementsTest() {
+        input = "    \n     1\nauthor\nte!!xt\n2\n2\ncountry\ntext2\n8\n\t\r\t\r3\nanswer\nte!!!!!xt2\n6\n2\ncountry\nte!xt2\n8\n  \t \n \r    1\nauthor\nte!!!xt2\n9\n   \r\r\r   \r\n\n\t\n\n    ";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.sort(slc);
+        SingleLinkedContainer.out(slc, new PrintWriter(output));
+        assertEquals(output.toString(), """
+                1: Proverb: text2. From: country. Rating score: 8\r
+                2: Proverb: te!xt2. From: country. Rating score: 8\r
+                3: Aphorism: te!!xt. By: author. Rating score: 2\r
+                4: Aphorism: te!!!xt2. By: author. Rating score: 9\r
+                5: Riddle: te!!!!!xt2. Answer: answer. Rating score: 6\r
+                """);
+    }
+
+    @Test
+    void sortEmptyTest() {
+        input = "";
+        SingleLinkedContainer.in(slc, new Scanner(input));
+        SingleLinkedContainer.sort(slc);
+        SingleLinkedContainer.out(slc, new PrintWriter(output));
+        assertEquals(output.toString(), "");
+    }
+}
